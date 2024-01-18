cc0cfc432014a49171bf09d39ec3c54f1bddddd2
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-18 12:44:24 -0800

    Done!

--- Person.cs
+++ Person.cs
@@ -8,5 +8,32 @@ namespace GA_ThisKeyword_StephanieLopez
 {
     internal class Person
     {
+        //Fields
+        private string name;
+        private int age;
+
+        //Constructor using "this" to differ fields from parameters
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //Method to display person's details
+        public void Display()
+        {
+            Console.WriteLine($"Name:{this.name}, Age:{this.age}");
+        }
+
+        //Overload constructor
+        public Person(string name) : this(name, 0) //Uses the main constructor
+        {
+        }
+
+        //This method will update person's age
+        public void SetAge(int age)
+        {
+            this.age = age; //"this" is used to clarify that we are assigning it to the instance field
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,25 @@
     internal class Program
     {
         //Stephanie Lopez
+        // 1/18/24
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person individual1 = new Person("Yolanda", 42);
+            individual1.Display();
+
+            Console.WriteLine("");
+            Person individual2 = new Person("Oswaldo");
+            individual2.Display();
+            individual2.SetAge(27);
+            individual2.Display();
+
+            Console.WriteLine("");
+            Person individual3 = new Person("Diana");
+            individual3.Display();
+            individual3.SetAge(19);
+            individual3.Display();
+
+            //The use of "this" in constructor is to differ fields from parameters as well as in methods. An example of "this" use could be constructor chains, property implementation and passing instances.
         }
     }
 }
